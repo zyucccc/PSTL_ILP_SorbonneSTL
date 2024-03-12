46603e10d0673b46db81c7dfe071b1f141953626
zhengdao <316321454@qq.com>
2024-03-12 23:13:54 +0100

    supprime inutilise array

--- Java/src/com/paracamplus/pstl/interfaces/IASTarray.java
+++
@@ -1,7 +0,0 @@
-package com.paracamplus.pstl.interfaces;
-
-import com.paracamplus.ilp1.interfaces.IASTexpression;
-
-public interface IASTarray extends IASTexpression{
-
-}
--- Java/src/com/paracamplus/pstl/interfaces/IASTarrayRead.java
+++
@@ -1,9 +0,0 @@
-package com.paracamplus.pstl.interfaces;
-
-import com.paracamplus.ilp1.interfaces.IASTexpression;
-
-public interface IASTarrayRead extends IASTarray{
-	public IASTexpression getTarget();
-	public IASTexpression getIndex();
-
-}
